36cc882e47ada9ccaf8c1541944a095e09e47cfe
ghigbie <gehigbie@gmail.com>
2019-03-17 09:35:41 -0600

    Add basic routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -11,3 +11,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data
 
 title = 'Flask with Database Relationships and Associations'
 
+@app.route('/')
+    def index():
+        return render_template('home.html', title=title)
+
+@app.route('/about')
+    def about():
+        return render_template('about.html', title=title)
+
+@errorhandler(404)
+    def page_not_found(e):
+        return render_template('notfound.html', title=title, e=e)
+
+if(__name__) == '__main__':
+    app.run(Debug=True)
