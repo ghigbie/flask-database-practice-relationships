7452e54c3c9141a104616181d86125ce977ed04b
ghigbie <gehigbie@gmail.com>
2019-03-17 10:09:12 -0600

    Edit readme file name

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+class Puppy(db.Model):
+    __tablename__ = "puppies"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    breed = db.Column(db.text)
 
 title = 'Flask with Database Relationships and Associations'
 
